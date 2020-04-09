a9b41033f101f8833ca5d6f515d4a9653a28054c
simpiis <simon.ejderbrand@hotmail.com>
2020-04-09 12:12:25 +0200

    test

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -14,6 +14,7 @@ public class Main extends Application {
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
+        System.out.println("swag");
     }
 
 
