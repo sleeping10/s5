436a885b43015c5ef5f8c9d43cedceb02f7a67cf
Ibrahim Ahmed Ali <ibbe2025@protonmail.com>
2020-04-14 14:07:32 +0200

    fxat lite i account och booking

--- src/sample/Booking.java
+++ src/sample/Booking.java
@@ -5,9 +5,37 @@ import java.util.Date;
 public class Booking {
     private int bookingID;
     private Date date;
-    private enum bookingType;
     private String bookingDesc;
+    public bookingType bookingType;
+    private int accountID;
 
-    //getters setters
+    enum bookingType {
+        Inspection, Service, Repair, Wash
+    }
+
+    public int getBookingID() {
+        return bookingID;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public String getBookingDesc() {
+        return bookingDesc;
+    }
+
+    public int getAccountID() {
+        return accountID;
+    }
+
+    public Booking(int bookingID, Date date, String bookingDesc, bookingType bookingType, int accountID) {
+        this.bookingID = bookingID;
+        this.date = date;
+        this.bookingDesc = bookingDesc;
+        this.bookingType = bookingType;
+        this.accountID = accountID;
+    }
+
 
 }
