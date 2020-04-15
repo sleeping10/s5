91cde37631161564e8d38c0efe839bd230f68b9c
feliixk <felixkivioja95@gmail.com>
2020-04-15 15:11:11 +0200

    Added basic code to Signup class

--- src/sample/SignUp.java
+++ src/sample/SignUp.java
@@ -1,17 +1,29 @@
 package sample;
 
-public class SignUp extends Verification {
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class SignUp extends Verification implements Initializable {
 
     public void signup(String email, String pass, String phoneNr, String name){
 
         if (verifyUser(email, pass) && verifyDetails(phoneNr, name)){
             //create new account here
+        }else{
+            //Error, either wrong input or already exist
         }
 
     }
 
     private boolean verifyDetails(String phoneNr, String name){
         return true;
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
     }
     //sign up class
 }
