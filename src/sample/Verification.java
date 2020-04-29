ff103f40ec1cf905eda987ca25f3e861b55a9c16
feliixk <felixkivioja95@gmail.com>
2020-04-29 23:14:06 +0200

    Changed verify method & added email regex check

--- src/sample/Verification.java
+++ src/sample/Verification.java
@@ -1,22 +1,37 @@
 package sample;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public abstract class Verification {
 
-    public int verifyAccount(String email, String pass, String phone){
+    public static final Pattern EMAIL_REGEX =
+            Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
+
+    public static int verifyAccount(String email, String pass, String phone){
         int status;
 
-        if (pass.length() > 8 || pass.length() < 4){
-            status = 2;
-        }else if(email.contentEquals("") || pass.contentEquals("")){
+        if(email.contentEquals("") || pass.contentEquals("")){
             status = 3;
-        }else{
+        }
+        else if (pass.length() > 8 || pass.length() < 4){
+            status = 2;
+        }
+        else if (!validate(email)){
+            status = 4;
+        }
+        else{
             if(DBC.getInstance().verifyAccount(email, pass, phone)){
                 status = 1;
             }else{
                 status = 0;
             }
         }
-        System.out.println("DEBUG: Verification status: " + status);
         return status;
+    }
+
+    public static boolean validate(String emailStr) {
+        Matcher matcher = EMAIL_REGEX.matcher(emailStr);
+        return matcher.find();
     }
 }
