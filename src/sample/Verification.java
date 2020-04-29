1d9031ad8da8fba8dfbe071756ff7f79d5ba5c1b
feliixk <felixkivioja95@gmail.com>
2020-04-29 22:15:41 +0200

    Reworked verify account method

--- src/sample/Verification.java
+++ src/sample/Verification.java
@@ -2,17 +2,21 @@ package sample;
 
 public abstract class Verification {
 
-    public boolean verifyAccount(String email, String pass, String phone){
-        boolean status = false;
+    public int verifyAccount(String email, String pass, String phone){
+        int status;
 
-        if(email.contentEquals("") || pass.contentEquals("")){
-            System.out.println("please type in something");
-            status = false;
+        if (pass.length() > 8 || pass.length() < 4){
+            status = 2;
+        }else if(email.contentEquals("") || pass.contentEquals("")){
+            status = 3;
         }else{
             if(DBC.getInstance().verifyAccount(email, pass, phone)){
-                status = true;
+                status = 1;
+            }else{
+                status = 0;
             }
         }
+        System.out.println("DEBUG: Verification status: " + status);
         return status;
     }
 }
