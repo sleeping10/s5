dd5e1d15e2d7000d65c70eea565775c3b9ce5b3e
feliixk <felixkivioja95@gmail.com>
2020-04-30 17:31:12 +0200

    Changes to ValidateEmail method

--- src/sample/Verification.java
+++ src/sample/Verification.java
@@ -5,22 +5,16 @@ import java.util.regex.Pattern;
 
 public abstract class Verification {
 
-    public static final Pattern EMAIL_REGEX =
-            Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
-
     public static int verifyAccount(String email, String pass, String phone){
         int status;
 
         if(email.contentEquals("") || pass.contentEquals("")){
             status = 3;
-        }
-        else if (pass.length() > 8 || pass.length() < 4){
+        } else if (pass.length() > 15 || pass.length() < 4){
             status = 2;
-        }
-        else if (!validate(email)){
-            status = 4;
-        }
-        else{
+        } else if (!validateEmail(email)){
+                status = 4;
+        } else{
             if(DBC.getInstance().verifyAccount(email, pass, phone)){
                 status = 1;
             }else{
@@ -30,7 +24,8 @@ public abstract class Verification {
         return status;
     }
 
-    public static boolean validate(String emailStr) {
+    public static boolean validateEmail(String emailStr) {
+        final Pattern EMAIL_REGEX = Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
         Matcher matcher = EMAIL_REGEX.matcher(emailStr);
         return matcher.find();
     }
