e76243a81bbb2ca324a1c9fe27f4e178cc98f3bf
feliixk <felixkivioja95@gmail.com>
2020-05-04 21:35:51 +0200

    update getters setters

--- src/sample/Account.java
+++ src/sample/Account.java
@@ -7,10 +7,9 @@ public class Account implements Serializable {
     private String password;
     private String name;
     private String phoneNr;
-    private int accountID = 0;
+    private int accountID;
     private int accessType;
     private boolean loginStatus;
-
 
     public String getEmail() {
         return email;
@@ -20,46 +19,20 @@ public class Account implements Serializable {
     public String getName() {
         return name;
     }
-
     public String getPhone() {
         return phoneNr;
     }
-
     public int getAccountID() {
         return accountID;
     }
-
     public int getAccessType() {
         return accessType;
     }
-
-    public void setAccessType(int a) {
-        this.accessType = a;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
     public void setPassword(String password) {
         this.password = password;
     }
-
     public String getPassword(){
         return this.password;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-
-    }
-
-    public void setPhoneNr(String phoneNr) {
-        this.phoneNr = phoneNr;
-    }
-
-    public void setAccountID(int accountID) {
-        this.accountID = accountID;
     }
 
     public Account(int accountID, String email, String password, String name, String phoneNr,boolean loginStatus, int accessType) {
@@ -71,10 +44,5 @@ public class Account implements Serializable {
         this.accessType = accessType;
         this.loginStatus = loginStatus;
 
-    }
-
-    @Override
-    public String toString() {
-        return this.email + ", " + this.password + ", " + this.name;
     }
 }
