52dc244a84ee8c061df53ddb6632811eb3cb687a
simpiis <simon.ejderbrand@hotmail.com>
2020-05-19 18:43:21 +0200

    website link on contact us

--- src/Controller/ContactUsController.java
+++ src/Controller/ContactUsController.java
@@ -1,9 +1,13 @@
 package Controller;
 
+import javafx.application.HostServices;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Hyperlink;
 import javafx.scene.control.Label;
 
+import java.awt.*;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -13,10 +17,12 @@ public class ContactUsController implements Initializable {
     @FXML private Label labelEmail;
     @FXML private Label labelPhone;
     @FXML private Label labelHours;
+    @FXML private Hyperlink hlWebsite;
 
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        hlWebsite.setText("Visit our website");
         labelAddress.setText("Elmetorpsvägen 15, 291 39 Kristianstad");
         labelEmail.setText("projektkurs2HKR@hotmail.com");
         labelHours.setText("Open 8:00 - 20:00 Every day");
@@ -25,4 +31,11 @@ public class ContactUsController implements Initializable {
     }
 
 
+    public void linkPressed(ActionEvent actionEvent) {
+        try {
+            Desktop.getDesktop().browse(new URL("https://hkr.se").toURI());
+        } catch (Exception ex){
+            ex.printStackTrace();
+        }
+    }
 }
