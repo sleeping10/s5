876a436cefe10fffd78203f38a7f15029369e063
feliixk <felixkivioja95@gmail.com>
2020-05-31 23:14:45 +0200

    code cleanup

--- src/Controller/SignupController.java
+++ src/Controller/SignupController.java
@@ -2,13 +2,8 @@ package Controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.*;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.*;
-import javafx.stage.Stage;
 import sample.*;
 
 import java.io.IOException;
